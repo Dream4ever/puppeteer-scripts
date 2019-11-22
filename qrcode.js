c9246477ec9e11f3afa544132491a196ad65cd90
Dream4ever <cnhwvista@gmail.com>
2019-11-22 15:37:42 +0800

    完善生成二维码的代码，识别的代码还需完善

--- qrcode.js
+++ qrcode.js
@@ -1,20 +1,45 @@
 var puppeteer = require('puppeteer');
 var config = require('./config/config');
 
+/*
+  TODO
+  目前生成二维码的代码没有问题
+  检查二维码的代码有问题
+  时不时地会出现已经上传新的二维码图片了
+  但代码读取到的二维码URL还是前一个图片的
+  然后就会报错，很闹心
+*/
 (async () => {
 
+  /* 设置浏览器启动参数 */
+  const width = 1920;
+  const height = 935;
+  const chrome = { x: 0, y: 74 };
+  // 设置启动后的窗口尺寸
+  // https://github.com/GoogleChrome/puppeteer/issues/1183
+  let args = [];
+  args.push(`--window-size=${width + chrome.x},${height + chrome.y}`);
+
   var browser = await puppeteer.launch({
-    // headless: false,
+    headless: false,
+    args,
     // devtools: true,
     // slowMo: 250,
   });
   console.log(`\n已启动浏览器\n`);
 
+
+  /* 设置新建标签页的参数 */
   var page = await browser.newPage();
+  await page.setViewport({
+    width,
+    height,
+  });
+  await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
   console.log(`已新建标签页\n`);
 
+  /* 从配置文件中读取需生成二维码的URL数组 */
   var urls = [];
-
   if (config.array && config.array.length > 0) {
     config.array.forEach(ele => {
       urls.push(`${config.baseUrl}${ele}.html`);
@@ -24,40 +49,44 @@ var config = require('./config/config');
       urls.push(`${config.baseUrl}${i}.html`);
     }
   }
-  
-  await page.setViewport({
-    width: 1920,
-    height: 935,
-  });
 
   for (var idx = 0; idx < urls.length; idx++) {
     await page.goto('https://cli.im/url');
 
     await page.waitForSelector('#url_content');
     console.log('01. 页面加载完毕');
-    await page.type(
-      '#url_content',
-      urls[idx], {
-        delay: 50,
-      }
+    // 草料网现在增加了检测机制，必须点击输入框之后才能模拟输入文字
+    await page.tap('#url_content');
+    await page.type('#url_content', urls[idx], { delay: 50, }
     );
 
     await page.waitForSelector('#click-create');
     await page.click('#click-create');
-    console.log('02. 二维码图片生成中');
-
+    // 输入网址生成二维码图片后，URL会变化，所以这一行不能省略
     await page.waitForNavigation();
+
+    await page.waitForSelector('li.col-md-3.col-sm-3.col-xs-3.first');
+    await page.click('li.col-md-3.col-sm-3.col-xs-3.first');
+    await page.waitForSelector('div#level')
+    await page.click('div#level')
+
+    await page.waitForSelector('a.dropdown-item[data-level="L"]')
+    await page.click('a.dropdown-item[data-level="L"]')
+
+    console.log('02. 二维码图片生成中');
 
     var qrcode = await page.$('#qrimage');
     console.log('03. 二维码图片已生成');
 
+    // 二维码图片非连续编号
     if (config.array && config.array.length > 0) {
       await qrcode.screenshot({
         path: `img/${config.array[idx]}.png`,
       });
       console.log(`04. 二维码图片 ${config.array[idx]} 已保存\n`);
-
-    } else if (config.startIndex) {
+    }
+    // 二维码图片连续编号
+    else if (config.startIndex) {
       await qrcode.screenshot({
         path: `img/${config.startIndex + idx}.png`,
       });
@@ -65,35 +94,48 @@ var config = require('./config/config');
     }
   };
 
+
   // 检查二维码
-  // TODO
-  // 试试下面的这个API
-  // https://pptr.dev/#?product=Puppeteer&version=v1.19.0&show=api-class-filechooser
-  await page.goto('https://hewei.in/decode-qrcode/', {
+  await page.goto('https://cli.im/deqr', {
     waitUntil: ['load', 'domcontentloaded', 'networkidle0', 'networkidle2']
   });
   console.log('页面已完全加载\n');
 
+  let allOK = true;
   for (var idx = 0; idx < urls.length; idx++) {
+    // console.log(1)
+    await page.waitForSelector('input#filedatacode');
+    const [fileChooser] = await Promise.all([
+      page.waitForFileChooser(),
+      page.click('input#filedatacode'),
+    ]);
+    // await Promise.all([
+    //   page.waitForFunction("document.querySelector('#deqrresult').innerText.length > 0"),
+    //   page.waitFor(1000),
+    //   fileChooser.accept([`img/${config.startIndex + idx}.png`])
+    // ])
+    await fileChooser.accept([`img/${config.startIndex + idx}.png`]);
+    // console.log(2)
+    
+    // await page.waitForFunction("document.querySelector('#deqrresult').innerText.length > 0")
+    // console.log(3)
 
-    await page.waitForSelector('input#upload');
-    const input = await page.waitForSelector('input#upload');
-    let fileName = config.array ? config.array[idx] : (config.startIndex + idx);
-    await input.uploadFile('img/' + fileName + '.png');
-    await page.waitForSelector('#decode');
-    await page.click('#decode');
+    const innerText = await page.evaluate(() => document.querySelector('#deqrresult').innerText);
+    // console.log(4)
 
-    await page.waitForSelector('#result');
-    const innerText = await page.evaluate(() => document.querySelector('#result').innerText);
     if (innerText !== urls[idx]) {
+      allOK = false;
       console.log('二维码图片内容有误');
-      console.log(`报错信息：${innerText}`);
-      console.log(`正确网址：${urls[idx]}`);
+      console.log(`innerText: ${innerText}`)
+      console.log(urls[idx])
       console.log('请为上面的网址重新生成二维码图片并再次检查');
       console.log('\n');
     }
   }
-  console.log('二维码图片全部检查完毕，图片全部正常可用 :)');
+  console.log('二维码图片全部检查完毕');
+  if (allOK) {
+    console.log('图片全部正常可用 :)')
+  }
 
   await browser.close();
   console.log(`\n浏览器已关闭`);
